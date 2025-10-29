719a50924ae4eb004bd0a904fddd5cdf9dc1c320
madinafz <madina.fayz@edu.fh-joanneum.at>
2025-10-29 10:32:54 +0100

    Übung 3
    Änderungen laut Anweisungen

--- src/main/java/at/fhj/Main.java
+++ src/main/java/at/fhj/Main.java
@@ -1,12 +1,20 @@
 package at.fhj;
 
+import java.sql.SQLOutput;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welcom!");
+        String k = "hallo";
+        String x = "keine Ahnung";
+        System.out.println(k+x);
+
+        //hallo, hier ist meine datei
+        //tschüss, das war's
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
--- src/main/java/at/fhj/MyClass.java
+++ src/main/java/at/fhj/MyClass.java
@@ -1,6 +1,7 @@
 package at.fhj;
 
 public class MyClass {
+
     public void doStuff(){
         System.out.println("doStuff");
     }
