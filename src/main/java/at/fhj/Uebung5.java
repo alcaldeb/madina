21a3e25ea239b5f0af6efc1f9ea8d23c63fcf383
alcaldeb <45821141+alcaldeb@users.noreply.github.com>
2025-10-29 10:53:57 +0100

    Solving u5
    new class with awesome method

---
+++ src/main/java/at/fhj/Uebung5.java
@@ -0,0 +1,12 @@
+package at.fhj;
+
+public class Uebung5 {
+    Ka myKa;
+    Ka myKa2;
+
+    //TODO: add constructor
+
+    public void combineKa() {
+        System.out.println(myKa.gemma+myKa2.gemma);
+    }
+}
